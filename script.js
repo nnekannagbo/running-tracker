2b8e62c3e7a54bce9f612ded2c8ac6b3244c7a2d
Nneka Nnagbo <nnekakassandra@gmail.com>
2020-09-01 20:47:44 -0400

    Add js

---
+++ script.js
@@ -0,0 +1,23 @@
+let entries = [];
+
+const entriesWrapper = document.querySelector('#entries');
+
+function addNewEntry(newEntry) {
+    entriesWrapper.removeChild(entriesWrapper.firstElementChild);
+    const listItem = document.createElement('li');
+    const listValue = document.createTextNode(newEntry);
+    listItem.appendChild(listValue);
+
+    entriesWrapper.appendChild(listItem);
+}
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const entry = Number(document.querySelector('#entry').value);
+    if (!entry) return;
+    document.querySelector('form').reset();
+    entries.push(entry);
+    addNewEntry(entry);
+}
+
+const form = document.querySelector('form').addEventListener('submit', handleSubmit);
